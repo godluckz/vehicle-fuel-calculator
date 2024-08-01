33bb64159728c3f6c850c2854c8716b651200e2f
Godluck Nsanya <GodluckN@miway.co.za>
2024-08-01 17:24:25 +0200

    Add calculation logic

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 
+def format_currency(p_amount):
+    return 'R{:,.2f}'.format(p_amount)
+
+def calculate_trip_cost(p_distance: int, 
+                        p_litters_per_100km: int, 
+                        w_fuel_price_per_litter: int) -> tuple[int,int]:
+    w_100s       = p_distance/100
+    w_litters    = w_100s * p_litters_per_100km
+    w_total_cost = w_litters * w_fuel_price_per_litter
+
+    return w_litters, w_total_cost
+
+
+def main():
+    print("****************************************")
+    print("!!Welcome to Trip-Fuel-Cost calculator!!")
+    print("****************************************")
+    w_distance          = int(input(f"What is the total trip distance (Km): "))
+    w_litters_per_100km = int(input(f"What is vehicles fuel consumption (L/100Km): "))
+    w_fuel_price        = int(input(f"What is the current fuel price (per litter): "))
+    
+
+    w_total_litters, w_total_cost = calculate_trip_cost(p_distance = w_distance,
+                                                        p_litters_per_100km = w_litters_per_100km,
+                                                        w_fuel_price_per_litter = w_fuel_price)
+    
+    print(f"\n\nFor the given vehicle with a consumption of {w_litters_per_100km} Litters/100Km, Trip of {w_distance}Km will require at least:\nFuel: {w_total_litters} Litters\nCost: {format_currency(w_total_cost)} (in Fuel currency, e.g Rands).\n")
+    print("-----------------------------------------")
+    print("-----------------------------------------")    
+    print("Have a safe Trip 🛣️🛻💃📷\n\n")
+
+
 
 if __name__ == "__main__":
     print("in main")
+    main()
